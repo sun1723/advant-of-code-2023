7c842ddcec2c4865047135ecf7b2d521c351cf33
Peiting Sun <peiting.sun@aligent.com.au>
2023-12-05 06:21:34 +1030

    day03 part1

---
+++ day03.mjs
@@ -0,0 +1,104 @@
+import fs from "fs";
+var array = fs.readFileSync("./input/input03.txt").toString().split("\n");
+
+/**
+ * check if symbols near by a number
+ * 1. find number -- regex
+ * 2. check surrounding , EG, number has startIdx: x1, endIdx: x2, currentRow: y => check range: horizontal (x1-1,y),(x2+1,y),(x1-1,y-1),(x-1,y+1),(x1...x2,y+1),(x1....x2,y-1);
+ * 3. if any, add to sum, if not, do nothing
+ */
+
+
+function extractNumbersAndSymbols(inputString) {
+    const regex = /(\d+(\^.\d*)?)|[^.\d]/g;
+    let match;
+    const result = [];
+  
+    while ((match = regex.exec(inputString)) !== null) {
+      const value = match[0];
+      const startIndex = match.index;
+      const endIndex = match.index + value.length - 1;
+  
+      if (value !== '.') {
+        result.push({
+          value,
+          startIndex,
+          endIndex,
+        });
+      }
+    }
+  
+    return result;
+  }
+
+const symbolsMap =[];
+for(let i =0; i<array.length; i++) {
+    const symbols = extractNumbersAndSymbols(array[i]);
+    
+    symbolsMap.push(symbols.reduce((acc,symbol) => {
+        // check if it is a symbol
+        if(isNaN(symbol.value)) {
+            acc.push(symbol.startIndex);
+        }
+        return acc;
+    },[]));
+}
+
+const numberMap={};
+for(let i =0; i<symbolsMap.length; i++) {
+    if(symbolsMap[i].length === 0) {
+        continue;
+    }
+    if(!numberMap[i-1]) {
+        numberMap[i-1]=[];
+    }
+    // y-1 row,TODO: check if it is row 0
+    numberMap[i-1]=[...new Set([...numberMap[i-1],...symbolsMap[i].reduce((acc, symbolIndex)=>{
+        acc.push(symbolIndex-1);
+        acc.push(symbolIndex);
+        acc.push(symbolIndex+1);
+        return acc;
+    },[])])]
+
+    if(!numberMap[i]) {
+        numberMap[i]=[];
+    }
+    // y row
+    numberMap[i]=[...new Set([...numberMap[i] ,...symbolsMap[i].reduce((acc, symbolIndex)=>{
+        acc.push(symbolIndex-1);
+        acc.push(symbolIndex+1);
+        return acc;
+    },[])])]
+    if(!numberMap[i+1]) {
+        numberMap[i+1]=[];
+    }
+    //y+1 row,TODO: check if it is the last line
+    numberMap[i+1]=[...new Set([...numberMap[i+1],...symbolsMap[i].reduce((acc, symbolIndex)=>{
+        acc.push(symbolIndex-1);
+        acc.push(symbolIndex);
+        acc.push(symbolIndex+1);
+        return acc;
+    },[])])]
+}
+
+let sum = 0;
+const res = [];
+// check numbers from original extraction
+for(let i =0; i<array.length; i++) {
+    const elements = extractNumbersAndSymbols(array[i]);
+    elements.map(element =>  {
+        const number =parseInt(element.value);
+        // check if it is a number
+        if(!isNaN(number)) {
+            for(const num of numberMap[i]){
+                if(num>=element.startIndex && num <= element.endIndex) {
+                    res.push(number);
+                    sum += number;
+                    break;
+                }
+            }
+        }
+    })
+}
+
+console.log(sum)
